9d53ec0fcee196a53cd2cc39d461a2414176de91
Ayc0 <ayc0.benj@gmail.com>
2018-12-29 17:58:46 +0100

    use forEach instead of for of

--- server/src/utils/verifySchema.js
+++ server/src/utils/verifySchema.js
@@ -19,17 +19,14 @@ const getSubscriptions = ({ definitions }) =>
     .reduce((names, subNames) => [...names, ...subNames], []);
 
 const verifySchema = (typeDefs, resolvers) => {
-  const queries = getQueries(typeDefs);
-  const mutations = getMutations(typeDefs);
-  const subscriptions = getSubscriptions(typeDefs);
-
   const toCheck = [
-    [queries, 'Query'],
-    [mutations, 'Mutation'],
-    [subscriptions, 'Subscription'],
+    [getQueries(typeDefs), 'Query'],
+    [getMutations(typeDefs), 'Mutation'],
+    [getSubscriptions(typeDefs), 'Subscription'],
   ];
   let errors = false;
-  for (let [types, Type] of toCheck) {
+
+  toCheck.forEach(([types, Type]) => {
     if (types.length !== 0) {
       if (!resolvers[Type]) {
         errors = true;
@@ -37,37 +34,39 @@ const verifySchema = (typeDefs, resolvers) => {
           kleur.yellow(`You need to implement ${kleur.bold().red(Type)}`),
         );
       } else {
-        for (let request of types) {
-          if (!(request in resolvers[Type])) {
-            errors = true;
-            console.warn(
-              kleur.yellow(
-                `You need to implement ${kleur
-                  .bold()
-                  .red(request)} in ${kleur.bold().red(Type)}`,
-              ),
-            );
+        types.forEach(request => {
+          if (request in resolvers[Type]) {
+            return;
           }
-        }
+          errors = true;
+          console.warn(
+            kleur.yellow(
+              `You need to implement ${kleur
+                .bold()
+                .red(request)} in ${kleur.bold().red(Type)}`,
+            ),
+          );
+        });
       }
     }
-  }
+  });
 };
 
 const verifyEmitters = (typeDefs, emitters) => {
   let errors = false;
   const subscriptions = getSubscriptions(typeDefs);
   for (let emitter in emitters) {
-    if (!subscriptions.includes(emitter)) {
-      errors = true;
-      console.warn(
-        kleur.yellow(
-          `You've never defined ${kleur
-            .bold()
-            .red(emitter)} in your subscriptions`,
-        ),
-      );
+    if (subscriptions.includes(emitter)) {
+      return;
     }
+    errors = true;
+    console.warn(
+      kleur.yellow(
+        `You've never defined ${kleur
+          .bold()
+          .red(emitter)} in your subscriptions`,
+      ),
+    );
   }
 };
 
