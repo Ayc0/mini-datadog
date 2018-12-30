2ddee4499e8a9f7d3639543d629d0120d6d8efc3
Ayc0 <ayc0.benj@gmail.com>
2018-12-30 01:31:54 +0100

    Add timers to check websites

--- server/src/controllers/monitoring.js
+++ server/src/controllers/monitoring.js
@@ -1,21 +1,35 @@
+const Results = require('./results');
+
 const monitorings = {};
 
-const addMonitoring = ({ url, minutes = 1, seconds = 0, millis = 0 }) => {
+const addMonitoring = ({ url, minutes = 0, seconds = 0, millis = 0 }) => {
   let checkInterval = (minutes * 60 + seconds) * 1000 + millis;
+  if (checkInterval === 0) {
+    // default is 60'
+    checkInterval = 60000;
+  }
   // max is 10'
   if (checkInterval > 600000) {
     checkInterval = 600000;
   }
+  // Update checkInterval value
   if (url in monitorings) {
     const monitoring = monitorings[url];
+    monitoring.stopRequestTimer();
     monitoring.checkInterval = checkInterval;
+    monitoring.startRequestTimer();
     return monitoring;
   }
-  const monitoring = { url, checkInterval };
+  // Create new monitoring
+  const monitoring = new Results(url, checkInterval);
   monitorings[url] = monitoring;
   return monitoring;
 };
 
-const getMonitorings = () => Object.values(monitorings);
+const getMonitorings = () =>
+  Object.values(monitorings).map(({ checkInterval, url }) => ({
+    checkInterval,
+    url,
+  }));
 
 module.exports = { addMonitoring, getMonitorings };
--- server/src/controllers/results.js
+++ server/src/controllers/results.js
@@ -1,15 +1,58 @@
+const interval = require('@libshin/interval');
+
 const axios = require('../utils/axios');
 const { emit } = require('../utils/actions');
 
+const TWO_MIN = 2 * 60 * 1000;
+const TEN_MIN = 10 * 60 * 1000;
+
 class Results {
-  constructor(url) {
+  constructor(url, checkInterval) {
+    // Bind functions in order to make "this." work consistently
+    this.startRequestTimer = this.startRequestTimer.bind(this);
+    this.stopRequestTimer = this.stopRequestTimer.bind(this);
+    this.stopTimers = this.stopTimers.bind(this);
+    this.performRequest = this.performRequest.bind(this);
+    this.saveResponse = this.saveResponse.bind(this);
+    this.summary = this.summary.bind(this);
+
     this.url = url;
+    this.checkInterval = checkInterval;
     this.requestTimer = null;
-    this.two;
+    this.twoMinTimer = interval(this.summary(true), TWO_MIN);
+    this.tenMinTimer = interval(this.summary(true), TEN_MIN);
     this.results = [];
+
+    this.startRequestTimer();
+    this.twoMinTimer.start();
+    this.tenMinTimer.start();
   }
+
+  startRequestTimer() {
+    this.stopRequestTimer();
+    this.requestTimer = interval(this.performRequest, this.checkInterval);
+    this.requestTimer.startNow();
+  }
+  stopRequestTimer() {
+    if (this.requestTimer) {
+      this.requestTimer.stop();
+      this.requestTimer = null;
+    }
+  }
+  stopTimers() {
+    this.stopRequestTimer();
+    if (this.twoMinTimer) {
+      this.twoMinTimer.stop();
+    }
+    if (this.tenMinTimer) {
+      this.tenMinTimer.stop();
+    }
+    this.twoMinTimer = null;
+    this.tenMinTimer = null;
+  }
+
   performRequest() {
-    axios(url)
+    axios(this.url)
       .then(res => this.saveResponse(res.status, res.duration))
       .catch(err => this.saveResponse(new Error(), err.duration));
   }
@@ -23,7 +66,7 @@ class Results {
     });
 
     // Don't keep results generate more than 10min ago
-    const tenMinAgo = instant - 600000;
+    const tenMinAgo = instant - TEN_MIN;
     const tenMinIndex = Math.max(
       this.results.findIndex(results => results.instant > tenMinAgo),
       0,
@@ -32,13 +75,13 @@ class Results {
   }
 
   summary(fast = false) {
-    const lookupDuration = fast ? 120000 : 600000; // 2min or 10min
+    const lookupDuration = fast ? TWO_MIN : TEN_MIN; // 2min or 10min
     return () => {
       const instant = Date.now();
       const beforeInstant = instant - lookupDuration;
       // We duplicate the results to avoid having a new response that could modify the list
       const results = this.results.filter(
-        results => results.instant > durationCheckup,
+        results => results.instant > lookupDuration,
       );
       // Sort by ascending duration
       results.sort((a, b) => a.duration - b.duration);
@@ -59,22 +102,28 @@ class Results {
         [0, 0, {}],
       );
       const maxStatus = Math.max(...Object.values(differentStatuses));
-      const mostFrequentStatus = Object.keys(differentStatuses).find(
-        status => differentStatuses[status] === maxStatus,
+      const mostFrequentStatus = parseInt(
+        Object.keys(differentStatuses).find(
+          status => differentStatuses[status] === maxStatus,
+        ),
+        10,
       );
-      emit.resultsPerformed({
-        lookupDuration,
+      const data = {
         url: this.url,
+        lookupDuration,
         mostFrequentStatus,
         totalRequests: results.length,
         totalDuration,
         totalErrors,
         averageServiceTime: totalDuration / results.length,
-        p50ServiceTime: results[Number(results.length * 0.5)].duration,
-        p90ServiceTime: results[Number(results.length * 0.9)].duration,
-        p95ServiceTime: results[Number(results.length * 0.95)].duration,
+        p50ServiceTime: results[parseInt(results.length * 0.5, 10)].duration,
+        p90ServiceTime: results[parseInt(results.length * 0.9, 10)].duration,
+        p95ServiceTime: results[parseInt(results.length * 0.95, 10)].duration,
         maxServiceTime: results[results.length - 1].duration,
-      });
+      };
+      emit.resultsPerformed(data);
     };
   }
 }
+
+module.exports = Results;
