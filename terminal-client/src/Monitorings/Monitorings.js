5c69b48b4bf4ff02e38b0610619c9a6797ee25bb
Ayc0 <ayc0.benj@gmail.com>
2018-12-31 15:07:50 +0100

    Add memo on monitorings

--- terminal-client/src/Monitorings/Monitorings.js
+++ terminal-client/src/Monitorings/Monitorings.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { memo } from 'react';
 
 import Monitoring from '../Monitoring';
 
@@ -10,4 +10,4 @@ const Monitorings = ({ monitorings }) => (
   </React.Fragment>
 );
 
-export default Monitorings;
+export default memo(Monitorings);
