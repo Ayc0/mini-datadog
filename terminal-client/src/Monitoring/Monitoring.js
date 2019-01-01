1034ee2901534cff1e48fcae17966348f8f6af80
Ayc0 <ayc0.benj@gmail.com>
2019-01-01 18:56:37 +0100

    Implement getLastMinMetrics to fetch at mount the metrics instead of waiting till next event

--- terminal-client/src/Monitoring/Monitoring.js
+++ terminal-client/src/Monitoring/Monitoring.js
@@ -1,66 +1,41 @@
 import React from 'react';
-import { withApollo } from 'react-apollo';
 import kleur from 'kleur';
 
-import subscription from '../gql/subscriptions/minMetricsPerformed.gql';
 import formatTime from '../utils/formatTime';
 import { statusColor } from '../utils/color';
 
-class Monitoring extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = { fastMetrics: null, slowMetrics: null };
-
-    props.client
-      .subscribe({
-        query: subscription,
-        variables: { url: props.url },
-      })
-      .forEach(({ data: { metricsPerformed } }) => {
-        if (metricsPerformed.fast) {
-          this.setState({ fastMetrics: metricsPerformed });
-        } else {
-          this.setState({ slowMetrics: metricsPerformed });
-        }
-      });
-  }
-
-  render() {
-    const { offset = 0, url, checkInterval } = this.props;
-    const { fastMetrics, slowMetrics } = this.state;
-    return (
-      <element
-        width="100%"
-        left="center"
-        top={offset * 3 + 4}
-        height={4}
-        border={{ type: 'line' }}
-        style={{ border: { fg: 'blue' } }}
-      >
-        <text top={0}>{`${url}  ${formatTime(checkInterval)} ↩️`}</text>
-        {fastMetrics && (
-          <text right={1} top={0}>
-            {kleur[statusColor(fastMetrics.lastStatus)](
-              fastMetrics.lastStatus === 0 ? 'Error' : fastMetrics.lastStatus,
-            )}
-          </text>
+const Monitoring = ({
+  offset = 0,
+  url,
+  checkInterval,
+  fastMetrics,
+  slowMetrics,
+}) => (
+  <element
+    width="100%"
+    left="center"
+    top={offset * 3 + 4}
+    height={4}
+    border={{ type: 'line' }}
+    style={{ border: { fg: 'blue' } }}
+  >
+    <text top={0}>{`${url}  ${formatTime(checkInterval)} ↩️`}</text>
+    {fastMetrics && (
+      <text right={1} top={0}>
+        {kleur[statusColor(fastMetrics.lastStatus)](
+          fastMetrics.lastStatus === 0 ? 'Error' : fastMetrics.lastStatus,
         )}
+      </text>
+    )}
 
-        <text left={4} bottom={0}>
-          {fastMetrics
-            ? `🐇 ${formatTime(fastMetrics.averageServiceTime)}`
-            : '🕑'}
-        </text>
+    <text left={4} bottom={0}>
+      {fastMetrics ? `🐇 ${formatTime(fastMetrics.averageServiceTime)}` : '🕑'}
+    </text>
 
-        <text left={14} bottom={0}>
-          {slowMetrics
-            ? `🐌 ${formatTime(slowMetrics.averageServiceTime)}`
-            : '🕑'}
-        </text>
-      </element>
-    );
-  }
-}
+    <text left={14} bottom={0}>
+      {slowMetrics ? `🐌 ${formatTime(slowMetrics.averageServiceTime)}` : '🕑'}
+    </text>
+  </element>
+);
 
-export default withApollo(Monitoring);
+export default Monitoring;
--- terminal-client/src/Monitoring/index.js
+++ terminal-client/src/Monitoring/index.js
@@ -1,1 +1,58 @@
-export { default } from './Monitoring';
+import React, { Component } from 'react';
+import { Query, withApollo } from 'react-apollo';
+
+import Monitoring from './Monitoring';
+
+import getLastMinMetrics from '../gql/queries/getLastMinMetrics.gql';
+import minMetricsPerformed from '../gql/subscriptions/minMetricsPerformed.gql';
+
+class MonitoringWithGQL extends Component {
+  constructor(props) {
+    super(props);
+
+    const { client, url } = props;
+
+    client
+      .subscribe({
+        query: minMetricsPerformed,
+        variables: { url },
+      })
+      .forEach(({ data }) => {
+        if (data && data.metricsPerformed) {
+          // Update cache and dispatch change to modify every components that use the getAlerts query
+          const query = { query: getLastMinMetrics, variables: { url } };
+          const cachedQuery = client.readQuery(query);
+          cachedQuery.getLastMetrics[
+            data.metricsPerformed.fast ? 'fast' : 'slow'
+          ] = data.metricsPerformed;
+          client.writeQuery({
+            ...query,
+            data: cachedQuery,
+          });
+        }
+      });
+  }
+
+  render() {
+    const { client, url, ...props } = this.props;
+    return (
+      <Query query={getLastMinMetrics} variables={{ url }}>
+        {({ data }) => {
+          if (!data || !data.getLastMetrics) {
+            return null;
+          }
+          return (
+            <Monitoring
+              {...props}
+              url={url}
+              slowMetrics={data.getLastMetrics.slow}
+              fastMetrics={data.getLastMetrics.fast}
+            />
+          );
+        }}
+      </Query>
+    );
+  }
+}
+
+export default withApollo(MonitoringWithGQL);
